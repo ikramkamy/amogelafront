270e55df0f3963994cc72cdf4919acee805d954e
ikramkamy <ikramzerkout13@gmail.com>
2021-08-03 10:34:11 +0200

    2

--- src/Components/Command/index.js
+++ src/Components/Command/index.js
@@ -7,18 +7,18 @@ const Command=()=>{
 <div className="bloc-command" style={{backgroundImage:"URL(/images/1.jpg)"}}>
     
     <span className="command-name"><h1>CLIQUEZ ET RETIREZ</h1>REGGIO EMILIA</span>
-    <div className="command-btn"><a href="/shop" >Scopri</a></div>
+    <div className="command-btn"><a href="/shop" >Commander</a></div>
     
 </div>
 <div className="bloc-command" style={{backgroundImage:"URL(/images/2.jpg)"}}>
 <span className="command-name"><h1>HOME DELIVERY</h1>
 REGGIO EMILIA</span>
-    <div className="command-btn"><a href="/shop">Scopri</a></div>
+    <div className="command-btn"><a href="/shop">Commander</a></div>
 </div>
 <div className="bloc-command" style={{backgroundImage:"URL(/images/3.jpg)"}}>
 <span className="command-name"><h1>SHOP <br/>ONLINE</h1>
 REGGIO EMILIA</span>
-    <div className="command-btn" ><a href="https://food.jumia.com.ng/">Scopri</a></div>
+    <div className="command-btn" ><a href="https://food.jumia.com.ng/">Commander</a></div>
    
 </div>
 
